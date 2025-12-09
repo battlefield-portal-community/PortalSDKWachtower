3db4b55abc943a82481b2d70ac437f3d30b7c8d8
MagneticNeedle <solankivibhakar82@gmail.com>
2025-12-10 01:24:17 +0530

    feat: send notif to discord when version changes

--- main.py
+++ main.py
@@ -1,14 +1,92 @@
 import asyncio
 import httpx
-import sys
 import os
 import json
+from datetime import datetime, UTC
+
+from dotenv import load_dotenv
+from discord_webhook import AsyncDiscordWebhook, DiscordEmbed
+from typing import List, Union
+
+load_dotenv()
 
 # Configuration from the YAML env
 LOCK_FILE = "version.lock"
 URL = "https://download.portal.battlefield.com/versions.json"
 # Using the specific User-Agent from the curl command to ensure consistent behavior
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:143.0) Gecko/20100101 Firefox/143.0 PortalSDKWachtower/https://github.com/battlefield-portal-community/PortalSDKWachtower"
+if not os.getenv("DISCORD_WEBHOOK_URL"):
+    print("DISCORD_WEBHOOK_URL environment variable not set. Exiting...")
+    exit(1)
+
+
+
+class HumanBytes:
+    METRIC_LABELS: List[str] = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+    BINARY_LABELS: List[str] = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"]
+    PRECISION_OFFSETS: List[float] = [0.5, 0.05, 0.005, 0.0005] # PREDEFINED FOR SPEED.
+    PRECISION_FORMATS: List[str] = ["{}{:.0f} {}", "{}{:.1f} {}", "{}{:.2f} {}", "{}{:.3f} {}"] # PREDEFINED FOR SPEED.
+
+    @staticmethod
+    def format(num: Union[int, float], metric: bool=False, precision: int=1) -> str:
+        """
+        Human-readable formatting of bytes, using binary (powers of 1024)
+        or metric (powers of 1000) representation.
+        """
+
+        assert isinstance(num, (int, float)), "num must be an int or float"
+        assert isinstance(metric, bool), "metric must be a bool"
+        assert isinstance(precision, int) and 0 <= precision <= 3, "precision must be an int (range 0-3)"
+
+        unit_labels = HumanBytes.METRIC_LABELS if metric else HumanBytes.BINARY_LABELS
+        last_label = unit_labels[-1]
+        unit_step = 1000 if metric else 1024
+        unit_step_thresh = unit_step - HumanBytes.PRECISION_OFFSETS[precision]
+
+        is_negative = num < 0
+        if is_negative: # Faster than ternary assignment or always running abs().
+            num = abs(num)
+
+        for unit in unit_labels:
+            if num < unit_step_thresh:
+                # VERY IMPORTANT:
+                # Only accepts the CURRENT unit if we're BELOW the threshold where
+                # float rounding behavior would place us into the NEXT unit: F.ex.
+                # when rounding a float to 1 decimal, any number ">= 1023.95" will
+                # be rounded to "1024.0". Obviously, we don't want ugly output such
+                # as "1024.0 KiB", since the proper term for that is "1.0 MiB".
+                break
+            if unit != last_label:
+                # We only shrink the number if we HAVEN'T reached the last unit.
+                # NOTE: These looped divisions accumulate floating point rounding
+                # errors, but each new division pushes the rounding errors further
+                # and further down in the decimals, so it doesn't matter at all.
+                num /= unit_step
+
+        return HumanBytes.PRECISION_FORMATS[precision].format("-" if is_negative else "", num, unit)
+
+async def send_discord_webhook(version: str, file_size: float, old_version: str, old_size: float) -> None:
+    file_size_readable = HumanBytes.format(file_size, metric=True)
+    size_change = HumanBytes.format(file_size - old_size, metric=True, precision=3)
+    if old_size < file_size:
+        size_change = f"+{size_change}"
+
+    webhook = AsyncDiscordWebhook(url=os.getenv("DISCORD_WEBHOOK_URL"))
+    embed = DiscordEmbed(username="Portal SDK Watchtower", color=0x00ff00)
+
+    embed.title = f"New Portal SDK Version Available!"
+    embed.set_thumbnail(url="https://lis.bfportal.gg/portal-animation-logo.gif")
+
+    embed.add_embed_field(name="New Version", value=f"`{old_version} -> {version}`")
+    embed.add_embed_field(name="File Size", value=f"{file_size_readable}")
+    embed.add_embed_field(name="", value="[Download](https://download.portal.battlefield.com/PortalSDK.zip)", inline=False)
+    if file_size - old_size != 0:
+        embed.add_embed_field(name="Size Change", value=f"`{size_change}`")
+    embed.set_timestamp(datetime.now(UTC))
+    embed.set_footer(text="Portal SDK Watchtower")
+
+    webhook.add_embed(embed)
+    await webhook.execute()
 
 async def get_version_details():
     headers = {'User-Agent': USER_AGENT}
@@ -32,7 +110,7 @@ async def get_version_details():
         
         return {
             "version": latest_entry.get('version'),
-            "fileSize": str(latest_entry.get('fileSize'))
+            "fileSize": int(latest_entry.get('fileSize'))
         }
 
 async def check_version(current_sdk_version: str, current_sdk_size: float):
@@ -58,6 +136,7 @@ async def check_version(current_sdk_version: str, current_sdk_size: float):
                 with open(LOCK_FILE, 'w') as f:
                     json.dump(details, f, indent=4)
                 print(f"Updated {LOCK_FILE} with new version info.")
+                await send_discord_webhook(new_version, new_size, current_sdk_version, current_sdk_size)
             except Exception as e:
                 print(f"Failed to update {LOCK_FILE}: {e}")
             return new_version, new_size
@@ -103,4 +182,5 @@ async def main():
             return
 
 if __name__ == "__main__":
+    # asyncio.run(send_discord_webhook("1.0.0", 6528632708, "0.0.9", 5528637863))
     asyncio.run(main())
